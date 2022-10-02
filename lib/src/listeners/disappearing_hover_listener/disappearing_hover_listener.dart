f0a9fcc530ee9a43b55ba62174b82c4ca7dc9f23
folaoluwfemi <folaoluwafemi55@gmail.com>
2022-10-02 22:58:55 +0100

    fix hover issue

--- lib/src/listeners/disappearing_hover_listener/disappearing_hover_listener.dart
+++ lib/src/listeners/disappearing_hover_listener/disappearing_hover_listener.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 class DisappearingHoverListener extends StatefulWidget {
   final bool escapeHover;
   final Widget child;
+  final double height;
+  final double width;
 
   ///double value between 0 and 1
   final double minOpacity;
@@ -17,6 +19,8 @@ class DisappearingHoverListener extends StatefulWidget {
     required this.escapeHover,
     required this.child,
     this.animationDuration = const Duration(milliseconds: 200),
+    required this.height,
+    required this.width,
   }) : super(key: key);
 
   @override
@@ -37,49 +41,46 @@ class _DisappearingHoverListenerState extends State<DisappearingHoverListener> {
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        return MouseRegion(
-          onHover: widget.escapeHover
-              ? (event) => setOpacityRelativeToHoverOffset(
-                    event.localPosition,
-                    Size(constraints.maxWidth, constraints.maxHeight),
-                  )
-              : null,
-          child: ValueListenableBuilder<double>(
-            valueListenable: opacityNotifier,
-            builder: (_, opacity, __) {
-              return AnimatedOpacity(
-                duration: duration,
-                opacity: opacity,
-                child: widget.child,
-              );
-            },
-          ),
-        );
-      },
+    return SizedBox(
+      width: widget.width,
+      height: widget.height,
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          return MouseRegion(
+            onHover: widget.escapeHover
+                ? (event) => setOpacityRelativeToHoverOffset(
+                      event.localPosition,
+                      Size(constraints.maxWidth, constraints.maxHeight),
+                    )
+                : null,
+            child: ValueListenableBuilder<double>(
+              valueListenable: opacityNotifier,
+              builder: (_, opacity, __) {
+                return AnimatedOpacity(
+                  duration: duration,
+                  opacity: widget.escapeHover ? opacity : 1.0,
+                  child: widget.child,
+                );
+              },
+            ),
+          );
+        },
+      ),
     );
   }
 
   ValueNotifier<double> opacityNotifier = ValueNotifier<double>(1.0);
 
-  bool isAnimating = false;
-
   void setOpacityRelativeToHoverOffset(
     Offset hoverOffset,
     Size widgetSize,
-  ) async {
-    if (isAnimating) return;
-    if (hoverOffset.isWithinBoundsOf(widgetSize)) {
+  ) {
+    if (hoverOffset.isWithinBoundsOf(widgetSize) &&
+        opacityNotifier.value != 0) {
       opacityNotifier.value = minOpacity;
-      isAnimating = true;
-      await Future.delayed(duration);
-      isAnimating = false;
-    } else {
+    }
+    if (hoverOffset.isAtExtremeOf(widgetSize)) {
       opacityNotifier.value = 1.0;
-      isAnimating = true;
-      await Future.delayed(duration);
-      isAnimating = false;
     }
   }
 }
--- lib/src/utils/extensions.dart
+++ lib/src/utils/extensions.dart
@@ -7,6 +7,8 @@ extension NumExtension on num {
 
   double get percentage => this * 100;
 
+  bool isBetween(num num1, num num2) => this >= num1 && this <= num2;
+
   double percent(double value) => (value / 100) * this;
 }
 
@@ -16,4 +18,16 @@ extension BuildContextExtension on BuildContext {
 
 extension OffsetExtension on Offset {
   bool isWithinBoundsOf(Size size) => dx <= size.width && dy <= size.height;
+
+  bool isAtExtremeOf(Size size, {double tolerance = 5}) =>
+      dx.isBetween(
+        size.width - tolerance,
+        size.width,
+      ) ||
+      dx.isBetween(0, tolerance) ||
+      dy.isBetween(0, tolerance) ||
+      dy.isBetween(
+        size.height - tolerance,
+        size.height,
+      );
 }
